39013d9c6859f74e5ddd059d87064d577c82a7fd
Emma Hack <ehack78@gmail.com>
2021-01-05 19:30:02 +0000

    calculator can add test passing

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,6 @@
+public class Calculator {
+
+    public int add(int numberOne, int numberTwo){
+        return numberOne + numberTwo;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator calculator;
+
+    @Before
+    public void setUp(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void canAdd(){
+        assertEquals(4, calculator.add(2, 2));
+    }
+}

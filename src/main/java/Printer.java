ca2a1e00d56a26b35e127a229ddcefdc26c683b1
Emma Hack <ehack78@gmail.com>
2021-01-05 20:19:49 +0000

    printer has pages test passed

---
+++ src/main/java/Printer.java
@@ -0,0 +1,11 @@
+public class Printer {
+    private int pages;
+
+    public Printer(int pages){
+        this.pages = pages;
+    }
+
+    public int getPages(){
+        return this.pages;
+    }
+}
---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrinterTest {
+
+    Printer printer;
+
+    @Before
+    public void setUp(){
+        printer = new Printer(25);
+    }
+
+    @Test
+    public void getPages(){
+        assertEquals(25, printer.getPages());
+    }
+
+//    @Test
+//    public void printEnoughPages(){
+//
+//    }
+//
+//    @Test
+//    public void printNotEnoughPages(){
+//
+//    }
+}
